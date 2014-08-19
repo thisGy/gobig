2131905940b19b5dcb8f992215805ef371cb4f18
Evan <meandur@gmail.com>
2014-08-19 06:40:57 -0400

    Updated the PHUL Model... Warmup almost completed...

--- model/PHULModel.php
+++ model/PHULModel.php
@@ -1,76 +1,148 @@
 <?php
+ // Workout Object
+/*
+	Workout -> PHUL -> Day ->Day 1 has Properties
+	Workout->PHUL->getDay->getExercises->getExerciseSets
+
+Day[1] => array(
+				'Exercise1' = array(
+							[Set2] => 
+							[Set3] => 
+							[Set4] => 							
+							)
+				'Exercis' = array(
+							[Set2] => 
+							[Set3] => 
+							[Set4] => 
+					)
+				)
+
+*/
+
+
 class PHUL extends Workout{
-	public $day1;
-	public $day2;
-	public $day4;
-	public $day5;
-
-	protected $day1_barbell_benchpress;
-	protected $day1_incline_db_bench_press;
-	protected $day1_bentover_row;
-	protected $day1_lat_pulldown;
-	protected $day1_overhead_press;
-	protected $day1_barbell_curl;
-	protected $day1_skullcrusher;
-
-	protected $day2_squat;
-	protected $day2_deadlift;
-	protected $day2_legpress;
-	protected $day2_legcurl;
-	protected $day2_calfexercise;
-
-
-	protected $day4_incline_barbell_benchpress;
-	protected $day4_flat_bench_dumbbell_flye;
-	protected $day4_seated_cable_row;
-	protected $day4_one_arm_dumbbell_row;
-	protected $day4_dumbbell_lateral_raise;
-	protected $day4_seated_incline_dumbbell_curl;
-	protected $day4_cable_tricep_extension;
-
-
-	protected $day5_front_squat;		
-	protected $day5_barbell_lunge;
-	protected $day5_leg_extension;
-	protected $day5_leg_curl;
-	protected $day5_seated_calf_raise;
-	protected $day5_calf_press;
+	public $day;
+	public $todays_workout;
 
 
 	function __construct(){
 		//Set Each Exercise Target Sets/Reps
 		
+		$this->setRepScheme();
+
+		$this->setWorkouts();
+
 	}
-	function setWorkoutDay1(){
+	function setWorkoutDay($day){
+		$this->day;
+	}
+	
+	function getWorkoutDay(){
+		return $this->day;
+
+	}
+
+	function getWorkoutofTheDay($day){
+		$this->getExercises($day);
+
 		return $day1;
 	}
-	function setWorkoutDay2(){
-		return $day2;
+	
+	function setRepScheme(){
+		$this->rep_scheme = array(
+			'3-4' => '3-5',
+			'3-4' => '6-10',
+			'2-3' => '5-8',
+			'2-3' => '6-10',
+			'3-5' => '10-15',
+			'4'   => '6-10',
+			'3-4' => '8-12',
+			'3-4' => '10-15',
+			);
+	}
+
+	function generateWorkoutSets(){
+		$rep_schemes = $this->rep_scheme;
+		foreach($rep_schemes as $sets => $reps){
+			if(is_numeric($sets)){
+				for($i=1; $i<=$sets; $i++){
+					$rules["".$sets."x".$reps.""]["Set ".$i.""] = $reps;
+				}
+			}else{
+				$set_range = explode('-',$sets);
+				$upper_limit = $set_range[1];
+				for($i=1; $i<=$upper_limit; $i++){
+					$rules["".$sets."x".$reps.""]["Set ".$i.""] = $reps;
+				}				
+			}
+		}
+		$this->rules = $rules;
+	}
+
+	function setWorkouts(){
+		$this->workouts = array(
+			'1' => array(
+					'Barbell Bench Press',
+					'Incline Dumbebell Bench Press',
+					'Bent Over Row',
+					'Lat Pulldown',
+					'Overhead Press',
+					'Barbell Curl',
+					'Skullcrusher'
+				),
+			'2' => array(
+					'Squat',
+					'Deadlift',
+					'Leg Press',
+					'Leg Curl',
+					'Calf Exercise',
+				),
+			'3' => array(
+					'Rest!  Go Drink! I mean... go for a run!',
+				),
+			'4' => array(
+					'Incline Barbell Bench Press',
+					'Flat Bench Dumbbell Flyes',
+					'Seated Cable Row',
+					'One Arm Dumbell Row',
+					'Dumbell Lateral Raise',
+					'Seated Incline Dumbbell Curl',
+					'Cable Tricept Extension'
+				),
+			'5' => array(
+					'Front Squat',
+					'Barbell Lunge',
+					'Leg Extension',
+					'Leg Curl',
+					'Seated Calf Raise',
+					'Calf Press',
+				),			
+			);
 
 	}
-	function setWorkoutDay4(){
-		return $day4;
 
-	}
-	function setWorkoutDay5(){
-		return $day5;
+	function getExercises(){
+		if ($day == 1){
+		//Power Upper Body
+				
+		}else if($day == 2){
+		//Power Lower Body
 
-	}
-	function getWorkoutDay1(){
-		return $day1;
-	}
-	function getWorkoutDay2(){
-		return $day2;
+		}else if($day == 3){	
+		// Rest, Go for a run!
 
+		}else if($day == 4){
+		//Hypertrophy Upper
+			
+		}else if($day == 5){
+		//Hypertrophy Lower
+		
+		}else{
+		// Invalid...	
+			$this->error .="Invalid Day. Please select a valid day.";
+		}
 	}
-	function getWorkoutDay3(){
-		return $day3;
 
-	}
-	function getWorkoutDay4(){
-		return $day4;
-
-	}
 
 
 
